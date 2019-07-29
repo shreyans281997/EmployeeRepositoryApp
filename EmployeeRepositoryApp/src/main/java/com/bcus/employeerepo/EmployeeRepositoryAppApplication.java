ca1ea29899ebee151ee76e4b15d842df7a68cc37
Shreyans <Shreyans@DESKTOP-4LMU8L5>
2019-07-29 23:16:58 +0530

    by shrey

---
+++ EmployeeRepositoryApp/src/main/java/com/bcus/employeerepo/EmployeeRepositoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.bcus.employeerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRepositoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRepositoryAppApplication.class, args);
+	}
+
+}
---
+++ EmployeeRepositoryApp/src/test/java/com/bcus/employeerepo/EmployeeRepositoryAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcus.employeerepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeRepositoryAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
